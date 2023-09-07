1f39ac9a0c4716f7ebc4a7d8b06ca0fdb9ec48e3
ishtiyak98 <rifatishtiyak@gmail.com>
2023-09-07 16:56:58 +0600

    flip-counter & gsap

--- GSAP/basic/main.js
+++ GSAP/basic/main.js
@@ -3,4 +3,4 @@
 //! gsap.set(".box", { transformOrigin: "50% 50%" });
 //! gsap.to(".box, .astronaut", { duration: 2, rotation: 360 });
 
-gsap.from(".box", {duration: 1.5, opacity: 0, scale: 0.3, ease: "back"})
+gsap.from(".box", { duration: 10, opacity: 0, scale: 0.3, ease: "back" });
